2ec8760fe26a3866e70e14fe0752dd28316954e5
Andrew Polk <andrew@polklabs.com>
2023-10-03 13:28:01 -0700

    Improved input for real player

--- Player_Real.py
+++ Player_Real.py
@@ -1,41 +1,54 @@
 from Player import Player
+import inquirer
 
 class PlayerReal(Player):
     # Return true to draw
     def DrawOrBank(self, nextData: dict, returnData: list):
-        r = input('Draw[1: Default] or Bank[2]:').lower()
-        if r in ['y', 'yes', '1', 'draw', '']:
-            return True
-        return False
+        questions = [
+        inquirer.List('draw',
+                        message="Draw Card or Bank Points?",
+                        choices=['Draw', 'Bank'],
+                    ),
+        ]
+        answers = inquirer.prompt(questions)
+        return answers['draw'] == 'Draw'
     
     # Return true to draw
     def DrawOrCall(self, nextData: dict, returnData: list):
-        r = input('Draw[1: Default] or Call[2]:').lower()
-        if r in ['y', 'yes', '1', 'draw', '']:
-            return True
-        return False
+        questions = [
+        inquirer.List('draw',
+                        message="Draw Card or Call?",
+                        choices=['Draw', 'Call'],
+                    ),
+        ]
+        answers = inquirer.prompt(questions)
+        return answers['draw'] == 'Draw'
     
-    def PlaceInStack(self, stacksIds: list[int], nextData: dict, returnData: list):
-        for i in stacksIds:
-            print(f'Stack {i+1}[{i+1}]')
-        while True:
-            try:
-                r = input('Choice:').lower()
-                if int(r)-1 in stacksIds:
-                    return int(r)-1
-            except:
-                print('Not valid')
+    def PlaceInStack(self, stackIds: list[int], nextData: dict, returnData: list):
+        choices = []
+        for i in stackIds:
+            choices.append(i+1)
+        questions = [
+        inquirer.List('place',
+                        message="Place card in stack?",
+                        choices=choices,
+                    ),
+        ]
+        answers = inquirer.prompt(questions)
+        return answers['place']-1
     
     def TakeStack(self, stackIds: list[int], nextData: dict, returnData: list):
+        choices = []
         for i in stackIds:
-            print(f'Stack {i+1}[{i+1}]')
-        while True:
-            try:
-                r = input('Choice:').lower()
-                if int(r)-1 in stackIds:
-                    return int(r)-1
-            except:
-                print('Not valid')
+            choices.append(i+1)
+        questions = [
+        inquirer.List('place',
+                        message="Take Stack?",
+                        choices=choices,
+                    ),
+        ]
+        answers = inquirer.prompt(questions)
+        return answers['place']-1
 
     def SleepTime(self, sleepType):
         return 0
